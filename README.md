0cb0c9d6541745a4640c018739278b25c0fad1bc
elijah.rich-wimmer <elijah.rich-wimmer@act-on.com>
2019-09-11 22:05:35 -0700

    Added current Composer development. code

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# Block-Supply-Composer
-Block-Supply application's source code using Hyperledger Composer
+# Block Supply Network
+
+## Deployment Instructions
+* Follow the [instalation instructions](https://hyperledger.github.io/composer/latest/installing/installing-prereqs) for Hyperledger Composer as outlined in the supplied link.
+    * Note: Composer requires version `8.12.0` of npm in order to propperly opperate.
+
+## Web App Instalation
+In a sepperate directory pull from the [git repository](REPLACEME). Follow the instructions provided.
+
+## Application Explanation:
---
+++ rebuild.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+source ~/.bash_profile 
+
+nvm use 8.12.0
+
+composer archive create -t dir -n .
+composer network install --card PeerAdmin@hlfv1 --archiveFile block-supply@0.0.6.bna
+
+composer network upgrade --networkName block-supply --networkVersion 0.0.6 --card PeerAdmin@hlfv1
+
+# composer network start --networkName block-supply --networkVersion 0.0.1 --networkAdmin admin --networkAdminEnrollSecret adminpw --card PeerAdmin@hlfv1 --file networkadmin.card
+# composer card import --file networkadmin.card
+composer network ping --card admin@block-supply
+
+composer-rest-server --explorer true --card admin@block-supply --n never -p 3000
+# composer-rest-server -c admin@block-supply -n never -d n -w true
